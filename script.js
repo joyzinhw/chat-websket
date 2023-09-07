9b70d0c6da41ac03816a5a5a69b460d0f4420b02
joyzinhw <joycemouraifpi@gmail.com>
2023-09-07 17:18:53 -0300

    serve

---
+++ script.js
@@ -0,0 +1,4 @@
+const port = 3001; // Altere isso para uma porta diferente
+app.listen(port, () => {
+  console.log(`O servidor está rodando na porta ${port}`);
+});
--- src/serve.ts
+++ src/serve.ts
@@ -1,14 +1,21 @@
-import express  from "express"
-import http from  'http'
+import express, { Application }  from "express"
+import http from 'http'
+import {Server} from "socket.io";
+
 class App {
+	private app: Application
+	private http: http.Server
+	private io: Server
 
 	constructor(){
-		this.app = express()
-		this.http  = http
+	 this.app = express()
+	 this.http  = http.createServer(this.app)
+	 this.io = new Server(this.http)
 	}
 
 	listenServer(){
 
+		this.http.listen(3000,() => console.log('server is running'));
 	}
 }
 
